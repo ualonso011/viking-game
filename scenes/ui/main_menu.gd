68a0c747a3a150515961fc2a483004e988e17851
ualonso011 <ualonso011@github.com>
2026-06-21 23:36:34 +0200

    fix: connect button signal via tscn instead of _ready() to avoid null reference errors

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -3,14 +3,6 @@ extends CanvasLayer
 
 signal start_game()
 
-@onready var title_label: Label = $MenuContainer/VBoxContainer/TitleLabel
-@onready var subtitle_label: Label = $MenuContainer/VBoxContainer/SubtitleLabel
-@onready var start_btn: Button = $MenuContainer/VBoxContainer/StartButton
-
-
-func _ready() -> void:
-	start_btn.pressed.connect(_on_start_pressed)
-
 
 func _on_start_pressed() -> void:
 	start_game.emit()

b0e6fdad522a8fe1a41c956887326438fa407785
ualonso011 <ualonso011@github.com>
2026-07-02 18:27:14 +0200

    feat(cutscenes): PR4 cutscenes + LevelBase refactor
    - Add portrait_panel.tscn, level_base.gd/.tscn
    - Extend cutscene_manager with portraits, backgrounds, ambience
    - Add new Spanish cutscenes for L3/L4/L6 and translate existing DB
    - Refactor all 7 levels to extend LevelBase
    - Add StoryTrigger nodes to L3/L4/L6; preserve L5/L7 boss logic
    - Mark PR4 tasks complete

--- scenes/levels/level_01.gd
+++ scenes/levels/level_01.gd
@@ -1,52 +1,21 @@
-extends Node2D
+extends LevelBase
 ## Level_01_Farm: tutorial/intro level.
 
-@onready var player: Node = $Player
-@onready var checkpoint: Area2D = $Checkpoint
-@onready var end_trigger: Area2D = $EndTrigger
 @onready var intro_trigger: Area2D = $IntroTrigger
 var intro_played: bool = false
 
 
 func _ready() -> void:
-	if checkpoint:
-		checkpoint.body_entered.connect(_on_checkpoint_entered)
-	if end_trigger:
-		end_trigger.body_entered.connect(_on_end_trigger_entered)
+	level_name = "Farm"
+	next_level_path = "res://scenes/levels/level_02.tscn"
+	checkpoint_unlocks_fury = true
+	super._ready()
+
 	if intro_trigger:
 		intro_trigger.body_entered.connect(_on_intro_trigger_entered)
-
-	var hud_node = get_node_or_null("/root/Main/HUD")
-	if hud_node and hud_node.has_method("show_level_name"):
-		hud_node.show_level_name("Farm", 3.0)
-
-	if player:
-		game_state.last_checkpoint = player.global_position
-		game_state.checkpoint_level = "res://scenes/levels/level_01.tscn"
 
 
 func _on_intro_trigger_entered(body: Node) -> void:
 	if body.is_in_group("player") and not intro_played:
 		intro_played = true
 		cutscene.play_cutscene(narrative_db.intro_farm())
-
-
-func _on_checkpoint_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		game_state.last_checkpoint = checkpoint.global_position
-		game_state.current_hp = game_state.max_hp
-		var hud_node = get_node_or_null("/root/Main/HUD")
-		if hud_node and hud_node.has_method("show_checkpoint"):
-			hud_node.show_checkpoint()
-		if not game_state.fury_unlocked:
-			game_state.fury_unlocked = true
-			game_state.fury_unlocked_changed.emit()
-
-
-func _on_end_trigger_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		game_state.add_level_damage_bonus()
-		game_state.current_level = 2
-		var main = get_node_or_null("/root/Main")
-		if main and main.has_method("load_level"):
-			game_manager.load_level("res://scenes/levels/level_02.tscn")
--- scenes/levels/level_06.gd
+++ scenes/levels/level_06.gd
@@ -1,40 +1,21 @@
-extends Node2D
+extends LevelBase
 ## Level_06_EnglandInvasion: Siege the English castle.
 
-@onready var player: Node = $Player
-@onready var checkpoint: Area2D = $Checkpoint
-@onready var end_trigger: Area2D = $EndTrigger
+@onready var story_trigger: Area2D = $StoryTrigger
+var story_played: bool = false
 
 
 func _ready() -> void:
-	if checkpoint:
-		checkpoint.body_entered.connect(_on_checkpoint_entered)
-	if end_trigger:
-		end_trigger.body_entered.connect(_on_end_trigger_entered)
+	level_name = "England Invasion"
+	next_level_path = "res://scenes/levels/level_07.tscn"
+	checkpoint_upgrades_hp = true
+	super._ready()
 
-	var hud = get_node_or_null("/root/Main/HUD")
-	if hud and hud.has_method("show_level_name"):
-		hud.show_level_name("England Invasion", 3.0)
-
-	if player:
-		game_state.last_checkpoint = player.global_position
-		game_state.checkpoint_level = "res://scenes/levels/level_06.tscn"
+	if story_trigger:
+		story_trigger.body_entered.connect(_on_story_trigger)
 
 
-func _on_checkpoint_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		game_state.last_checkpoint = checkpoint.global_position
-		game_state.current_hp = game_state.max_hp
-		var hud = get_node_or_null("/root/Main/HUD")
-		if hud and hud.has_method("show_checkpoint"):
-			hud.show_checkpoint()
-
-
-func _on_end_trigger_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		game_state.add_level_damage_bonus()
-		game_state.current_level = 7
-		game_state.upgrade_max_hp(1)
-		var main = get_node_or_null("/root/Main")
-		if main and main.has_method("load_level"):
-			game_manager.load_level("res://scenes/levels/level_07.tscn")
+func _on_story_trigger(body: Node) -> void:
+	if body.is_in_group("player") and not story_played:
+		story_played = true
+		cutscene.play_cutscene(narrative_db.england_intro(), &"rain")
